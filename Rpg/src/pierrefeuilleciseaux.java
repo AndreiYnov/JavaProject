8b05db1778f1d5fb1157bb684bc437bbf1ed9ea8
AndreiYnov <andrei.radocea@ynov.com>
2017-07-04 16:30:38 +0200

    pierre feuille ciseaux

---
+++ Rpg/src/pierrefeuilleciseaux.java
@@ -0,0 +1,11 @@
+
+public class pierrefeuilleciseaux {
+
+	public static void main(String[] args) {
+	    {
+	            //Annonce du début  
+	           System.out.println("Jeu de pierre feuille ciseau" );
+	             
+	}
+}	    
+}
