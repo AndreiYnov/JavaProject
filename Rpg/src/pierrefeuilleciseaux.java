64ca9d1840548d780c3f8dbb205e3fb9858d50af
AndreiYnov <andrei.radocea@ynov.com>
2017-07-04 16:49:31 +0200

    jeu

--- Rpg/src/pierrefeuilleciseaux.java
+++ Rpg/src/pierrefeuilleciseaux.java
@@ -1,11 +1,19 @@
+//Ceci importe la classe Scanner du package java.util
+import java.util.Scanner; 
 
 public class pierrefeuilleciseaux {
+
+	private static Scanner sc;
 
 	public static void main(String[] args) {
 	    {
 	            //Annonce du début  
 	           System.out.println("Jeu de pierre feuille ciseau" );
 	             
+	           sc = new Scanner(System.in);
+	           System.out.println("Veuillez saisir un mot :");
+	           String str = sc.nextLine();
+	           System.out.println("Vous avez saisi : " + str);
 	}
 }	    
 }
